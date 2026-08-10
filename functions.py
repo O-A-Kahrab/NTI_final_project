765fe2122de8af7a2a8a74f1d4a5173e12aedfa8
O-A-Kahrab <250437471+O-A-Kahrab@users.noreply.github.com>
2026-08-10 23:10:56 +0300

    added some changes to exact_search added a lst of possible extentions

--- functions.py
+++ functions.py
@@ -6,11 +6,18 @@ def Exact_Search(file_name,file_type,directory):
        and possible directories that may contain 
        the file. returns the directory of the file 
        if found otherwise return None"""
-    file_name = file_name+file_type 
+    if file_type != None: #to make sure it is not a folder
+        file_name = file_name+file_type 
     #to examine name and type at the same time
+
     for root,dirs,files in os.walk(directory):
         #walk returns the (the directory inserted, a list of all sub-directories, a list of all names for the files in the directory)
         #The loop will help examine each and every sub-directories and all there sub-directories
+        
         if file_name in files:
             return os.path.join(root,file_name)
     return None
+
+def advanced_file_search(file_name,file_type,directory):
+    pass
+
--- main.py
+++ main.py
@@ -1,16 +1,109 @@
 from functions import *
-
+import os
 file_name = input("Enter the exact name of the file: ") #target file
 
-# print("1-folder    2-.exe    3-.pptx\n4-.txt    5-.dll    6-jpg")#the print and the list will be continued by ai
-# n = input("Enter a number for one of the previous file types: ")
-# #n will be an index 
-# types = ["folder",".exe",]
-#file_type = types[n - 1] #instead of if statments
 
-file_type = input("type: ")
-directories = input("Enter directories to search in (seperate them using slashes -\\-): ") #list of directories
+extention = input("Enter the extention (enter -1 for folder): ")
+#n will be an index 
+file_extensions = [
+    ".3gp",
+    ".7z",
+    ".aac",
+    ".apk",
+    ".avi",
+    ".bat",
+    ".bin",
+    ".bmp",
+    ".bz2",
+    ".c",
+    ".cpp",
+    ".cs",
+    ".css",
+    ".csv",
+    ".dll",
+    ".doc",
+    ".docx",
+    ".epub",
+    ".exe",
+    ".flac",
+    ".flv",
+    ".gif",
+    ".gz",
+    ".heic",
+    ".htm",
+    ".html",
+    ".ico",
+    ".java",
+    ".jpeg",
+    ".jpg",
+    ".js",
+    ".json",
+    ".log",
+    ".m4a",
+    ".m4v",
+    ".md",
+    ".mid",
+    ".midi",
+    ".mkv",
+    ".mov",
+    ".mp3",
+    ".mp4",
+    ".msi",
+    ".ods",
+    ".odt",
+    ".ogg",
+    ".pdf",
+    ".php",
+    ".png",
+    ".ppt",
+    ".pptx",
+    ".psd",
+    ".py",
+    ".rar",
+    ".rb",
+    ".rtf",
+    ".sh",
+    ".sql",
+    ".svg",
+    ".sys",
+    ".tar",
+    ".tif",
+    ".tiff",
+    ".ts",
+    ".tsv",
+    ".txt",
+    ".wav",
+    ".webp",
+    ".wma",
+    ".wmv",
+    ".xls",
+    ".xlsx",
+    ".xml",
+    ".yaml",
+    ".yml",
+    ".zip",
+]
+#file_type = file_extensions[n - 1] #instead of if statments
+while True:
+    count = 0
+    directories = input("Enter directories to search in (seperate them using slashes -/-): ") #list of directories
+
+    for dir in directories.split("/"):
+        if os.path.isdir(dir):
+            count += 1
+
+    if count == len(directories.split("/")):
+        break
+    else:
+        print("Invalid directory")
+
 for directory in directories.split(sep="/"):
-    found_directory = Exact_Search(file_name,file_type,directory)
-print("The file was found at:\n\t",found_directory)
+    found_directory = Exact_Search(file_name,extention,directory)
+    
+if found_directory == None:
+    print("File not found")
+else:
+    print("The file was found at:\n\t",found_directory)
 
+#we can add search history as .json file ot just .txt file
+#for json the searched name is the key and its content is a list of returned directories
